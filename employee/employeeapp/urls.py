0962bb5db5c6a26b4285ed0e640c800c7e73486f
hospital <roshnisat9119@gmail.com>
2023-06-02 22:39:31 +0530

    first commit

---
+++ employee/employeeapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path,include
+from .import views
+
+urlpatterns = [
+    path('',views.employee_form ,name="employee_insert"),
+    path('list/',views.employee_list,name="employee_list"),
+    path('delete/<int:id>/',views.employee_delete,name="employee_delete"),
+    path('<int:id>/',views.employee_form,name="employee_update")
+    
+]
